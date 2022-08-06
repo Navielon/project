503ac68fc810cb65ab48458c687ffe5c5510f282
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-08-06 23:10:53 +0300

    Create 404.vue

---
+++ views/404.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <div class="page_header">
+        <div class="page_text_header">
+            Страница не найдена
+        </div>
+    </div>
+
+    <center>
+      <img src="/assets/img/404.png" /><br />
+      Такой страницы не существует.<br />
+      Если вы считаете, что это ошибка, сообщите администратору.
+    </center>
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Error',
+    methods: {}
+  }
+</script>
