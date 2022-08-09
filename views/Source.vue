95aae1da92469f494542cf81b0481f20c4a83e03
Ruslan_gg <107236487+Navielon@users.noreply.github.com>
2022-08-09 14:58:43 +0300

    Add files via upload

---
+++ views/Source.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    
+</div>
+</template>
+
+<script>
+    import * as main from '@/main.js';
+    import { EventBus } from '@/main.js';
+    export default { 
+        name: 'Source',
+        mounted() {
+            var source = this.$route.params.source;
+            this.$store.commit('source', {
+                src: source,
+            });
+            this.$router.push('/start');
+            this.$root.$emit('send_websocket', `{"packet":"source.redirect","data":{"url": "${source}"}}`);
+        },
+    }
+</script>
